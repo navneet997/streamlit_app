a4f5653e36d3022a06bb35c0c89e1a8eca3cd08d
navneet997 <139157098+navneet997@users.noreply.github.com>
2023-07-10 22:28:14 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,10 @@
 import streamlit
+import pandas
 streamlit.title(' New Healthy Diner App')
 streamlit.header('🥗 Breakfast Menu')
 streamlit.text(' 🥑Pohe')
 streamlit.text('🍞Paranthe')
 streamlit.text('🥣Magie')
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(my_fruit_list)
